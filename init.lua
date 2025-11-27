da80c8d23d860fcee84654af4bd90224656e0be1
thakyZ <950594+thakyZ@users.noreply.github.com>
2025-11-27 10:26:29 -0800

    fix: issue with packages

--- init.lua
+++ init.lua
@@ -104,6 +104,39 @@ local plugins = {
         LSPLoading6 = "⠴", LSPLoading7 = "⠦", LSPLoading8 = "⠧", LSPLoading9 = "⠇", LSPLoading10 = "⠏",
   },},},
 
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = { 'saghen/blink.cmp' },
+
+    -- Example using `opts` for defining servers
+    opts = {
+      servers = {
+        lua_ls = {
+          ["Lua.format.enable"] = false
+        }
+      }
+    },
+    config = function(_, opts)
+      local lspconfig = require('lspconfig')
+
+      for server, config in pairs(opts.servers) do
+        -- Passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end,
+
+    --[[ Example calling setup directly for each LSP
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local lspconfig = require('lspconfig')
+
+      lspconfig['lua_ls'].setup({ capabilities = capabilities })
+    end,
+    ]]--
+  },
+
   {"ray-x/lsp_signature.nvim", event = "BufRead", config = function() require("lsp_signature").setup(({hint_prefix="• "})) end,}, -- hints
   {"goolord/alpha-nvim",
     opts = function(_, opts)
@@ -141,7 +174,6 @@ local plugins = {
   },
   {"nikolvs/vim-sunbather"  },
   {"Mofiqul/vscode.nvim"    },
-  {"nocksock/onedark-vim"    },
   {"fcpg/vim-orbital"       },
   -- Diff
   {"tpope/vim-fugitive", cmd = { "Git", "G" }},
@@ -169,7 +201,40 @@ local plugins = {
   },
   { -- optional blink completion source for require statements and module annotations
     "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
         -- add lazydev to your completion providers
         default = { "lazydev", "lsp", "path", "snippets", "buffer" },
@@ -182,7 +247,19 @@ local plugins = {
           },
         },
       },
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = {
+        implementation = "prefer_rust",
+        prebuilt_binaries = {
+          force_version = "1.8.0",
+        },
+      },
     },
+    opts_extend = { "sources.default" }
   },
   --[[
   { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
